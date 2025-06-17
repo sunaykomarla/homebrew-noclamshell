843fbfcdacbdb2f1e2a5c4ccaa1bd58d83b49b70
Sunay Komarla <sunayrkomarla@gmail.com>
2025-06-17 14:27:44 -0400

    Update noclamshell.rb

--- Formula/noclamshell.rb
+++ Formula/noclamshell.rb
@@ -1,8 +1,8 @@
 class Noclamshell < Formula
   desc "Sleep despite annoying clamshell mode"
-  homepage "https://github.com/pirj/noclamshell"
-  url "https://github.com/pirj/noclamshell.git", tag: "1.3", revision: "df2b07dddd47a259d6306dfbb012f9f31b3df761"
-  head "https://github.com/pirj/noclamshell.git", branch: "master"
+  homepage "https://github.com/sunaykomarla/noclamshell"
+  url "https://github.com/sunaykomarla/noclamshell.git", tag: "1.3", revision: "df2b07dddd47a259d6306dfbb012f9f31b3df761"
+  head "https://github.com/sunaykomarla/noclamshell.git", branch: "master"
 
   def install
     bin.install "noclamshell"
